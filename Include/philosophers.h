69f9c24216114e04841681f158b4b07735251772
Saiban0 <bartched@outlook.fr>
2024-09-03 20:37:58 +0200

    + | Added tim to s_main and times_eaten to s_philo

--- Include/philosophers.h
+++ Include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:52:49 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/03 16:31:39 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/03 20:28:24 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,19 @@
 
 typedef struct s_main
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosophers_must_eat;
+	int			number_of_philosophers;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			number_of_times_each_philosophers_must_eat;
+	long long	time;
 }				t_main;
 
 typedef struct s_philo
 {
 	int	id;
 	int	alive;
+	int	times_eaten;
 }				t_philo;
 
 #endif
