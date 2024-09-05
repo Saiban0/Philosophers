2b673e54e4b04678e6bc919cfc79242a64f79877
Saiban0 <bartched@outlook.fr>
2024-09-05 20:09:12 +0200

    + | Added function to cleanup mutexes and threads

--- src/philo_error.c
+++ src/philo_error.c
@@ -6,16 +6,33 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:07:49 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/04 20:00:16 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/05 18:09:03 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+static void	destroy_fork_mutexes_and_threads(t_main *main)
+{
+	int	i;
+
+	i = 0;
+	while (i < main->number_of_philosophers)
+	{
+		put_back_forks(main, main->philo_list[i]);
+		pthread_mutex_destroy(&main->fork_list[i]->mutex);
+		pthread_join(main->philo_list[i]->thread, NULL);
+		i++;
+	}
+}
+
 static void	error_free(t_main *main)
 {
 	if (main->philo_list)
+	{
+		destroy_fork_mutexes_and_threads(main);
 		ft_free_double_array((void **)main->philo_list);
+	}
 	if (main->fork_list)
 		ft_free_double_array((void **)main->fork_list);
 	free(main);
