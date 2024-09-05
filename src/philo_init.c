0a64f5ea6f93aed6dc400925dcca91a8fe86e50c
Saiban0 <bartched@outlook.fr>
2024-09-05 20:09:53 +0200

    + | philo ands fork ids now range from 1 to max

--- src/philo_init.c
+++ src/philo_init.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:25:44 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/04 20:37:01 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/05 19:55:45 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static t_philo	*create_philo(int id)
 	t_philo	*new;
 
 	new = malloc(sizeof(t_philo));
-	new->id = id;
+	new->id = id + 1;
 	new->alive = 1;
 	new->times_eaten = 0;
 	new->forks_held = 0;
@@ -45,9 +45,11 @@ static t_fork	*create_fork(int id)
 	t_fork	*new;
 
 	new = malloc(sizeof(t_fork));
-	new->id = id;
+	new->id = id + 1;
 	new->available = 1;
 	new->used_by = -1;
+	ft_printf("%d %d\n", new->id, new->available);
+	pthread_mutex_init(&new->mutex, NULL);
 	return (new);
 }
 
@@ -61,7 +63,7 @@ int	create_fork_list(t_main *main)
 		main->fork_list[i] = create_fork(i);
 		i++;
 	}
-	main->fork_list[i] = NULL;
+	main->fork_list[i + 1] = NULL;
 	if (i != main->number_of_philosophers)
 		return (1);
 	return (0);
@@ -74,7 +76,6 @@ int	init_philo(t_main *main, int argc, char **argv)
 	main->time_to_eat = ft_atoi(argv[3]);
 	main->time_to_sleep = ft_atoi(argv[4]);
 	main->number_of_times_each_philosophers_must_eat = -1;
-	main->time = 0;
 	main->tmp = -1;
 	main->philo_list = malloc(main->number_of_philosophers * sizeof(t_philo *));
 	main->fork_list = malloc(main->number_of_philosophers * sizeof(t_fork *));
