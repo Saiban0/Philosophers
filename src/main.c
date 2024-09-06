9d0c4e732109763c77c3a679c75e809361ca0177
Saiban0 <bartched@outlook.fr>
2024-09-06 17:01:50 +0200

    - | removed useless return line

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:50:01 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/04 19:17:38 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/06 15:27:44 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ int	main(int argc, char **argv)
 		return (-1);
 	if (argc < 4)
 		return (1);
-	if (init_philo(main, argc, argv) == 1)
-		return (1);
+	init_philo(main, argc, argv);
 	philo_loop(main);
 	philo_end(main);
 	return (0);
