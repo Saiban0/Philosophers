d4a8d3c7ed06ada966d87a7257ccf86d51e432b8
Saiban0 <bartched@outlook.fr>
2024-09-09 19:47:40 +0200

    ~ | added new main mutex and 2 new varaibles to philo struct

--- Include/philosophers.h
+++ Include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:52:49 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/06 17:00:23 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/09 18:37:55 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ typedef struct s_philo
 	int				alive;
 	int				times_eaten;
 	int				forks_held;
+	int				last_meal;
 	pthread_t		thread;
 	pthread_mutex_t	mutex;
+	struct s_main	*main;
 }				t_philo;
 
 typedef struct s_fork
@@ -39,14 +41,15 @@ typedef struct s_fork
 
 typedef struct s_main
 {
-	int			number_of_philosophers;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			tmp;
-	int			number_of_times_each_philosophers_must_eat;
-	t_philo		**philo_list;
-	t_fork		**fork_list;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				tmp;
+	int				number_of_times_each_philosophers_must_eat;
+	t_philo			**philo_list;
+	t_fork			**fork_list;
+	pthread_mutex_t	mutex;
 }				t_main;
 
 typedef enum s_error_management
@@ -54,6 +57,7 @@ typedef enum s_error_management
 	e_none,
 	e_init_failure,
 	e_philo_died,
+	e_args,
 }			t_error;
 
 int		init_philo(t_main *main, int argc, char **argv);
@@ -61,12 +65,12 @@ void	error_management(int error_code, t_main *main);
 int		create_philo_list(t_main *main);
 int		is_a_philo_dead(t_main *main);
 int		has_every_philo_eaten(t_main *main);
-void	*philosopher_routine(void *data);
 void	philo_loop(t_main *main);
 void	philo_end(t_main *main);
 void	error_management(int error_code, t_main *main);
 void	philo_eat(t_main *main, t_philo *philo);
 void	put_back_forks(t_main *main, t_philo *philo);
 void	philo_think(t_main *main, t_philo *philo);
+void	philo_sleep(t_main *main, t_philo *philo);
 
 #endif
