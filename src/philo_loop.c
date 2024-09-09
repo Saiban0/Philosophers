5b13528046cfafdd8bbe8913c9a7b223138136b2
Saiban0 <bartched@outlook.fr>
2024-09-09 19:48:39 +0200

    ~ | reworked main loops and philos can now die

--- src/philo_loop.c
+++ src/philo_loop.c
@@ -6,39 +6,59 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:33:26 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/06 16:55:34 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/09 19:31:43 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static void	limited_routine(t_main *main, int i)
+static void	time_since_last_meal(t_main *main, t_philo *philo)
 {
-	while (has_every_philo_eaten(main) && is_a_philo_dead(main))
-	{
-		i = 0;
-		while (main->philo_list[i])
-		{
-			if (main->philo_list[i]->alive)
-			{
-				main->tmp = i;
-				philosopher_routine(main);
-			}
-			i++;
-		}
-	}
+	struct timeval	time;
+	int				diff;
+
+	gettimeofday(&time, NULL);
+	diff = (time.tv_sec / 1000) - philo->last_meal;
+	if (diff > main->time_to_die)
+		philo->alive = 0;
 }
 
-void	*philosopher_routine(void *data)
+static void	*limited_routine(void *data)
 {
-	t_main	*maint;
+	t_philo	*philo;
+	t_main	*main;
 
-	maint = (t_main *)data;
-	pthread_mutex_lock(&maint->philo_list[maint->tmp]->mutex);
-	philo_eat(maint, maint->philo_list[maint->tmp]);
-	// philo_think(main, main->philo_list[main->tmp]);
-	pthread_mutex_unlock(&maint->philo_list[maint->tmp]->mutex);
-	return (maint);
+	philo = (t_philo *)data;
+	main = philo->main;
+	while (!is_a_philo_dead(main) && has_every_philo_eaten(main))
+	{
+		// pthread_mutex_lock(&philo->mutex);
+		philo_eat(main, philo);
+		philo_think(main, philo);
+		philo_sleep(main, philo);
+		time_since_last_meal(main, philo);
+		// pthread_mutex_unlock(&philo->mutex);
+	}
+	return (main);
+}
+
+static void	*philosopher_routine(void *data)
+{
+	t_philo	*philo;
+	t_main	*main;
+
+	philo = (t_philo *)data;
+	main = philo->main;
+	while (!is_a_philo_dead(main))
+	{
+		pthread_mutex_lock(&philo->mutex);
+		philo_eat(main, philo);
+		philo_think(main, philo);
+		philo_sleep(main, philo);
+		time_since_last_meal(main, philo);
+		pthread_mutex_unlock(&philo->mutex);
+	}
+	return (main);
 }
 
 void	philo_loop(t_main *main)
@@ -48,21 +68,20 @@ void	philo_loop(t_main *main)
 	i = 0;
 	if (main->number_of_times_each_philosophers_must_eat == -1)
 	{
-		while (!is_a_philo_dead(main))
+		while (i < main->number_of_philosophers)
 		{
-			i = 0;
-			while (main->philo_list[i])
-			{
-				if (main->philo_list[i]->alive)
-				{
-					main->tmp = i;
-					pthread_create(&main->philo_list[i]->thread, NULL,
-						philosopher_routine, main);
-				}
-				i++;
-			}
+			pthread_create(&main->philo_list[i]->thread, NULL,
+				philosopher_routine, main->philo_list[i]);
+			i++;
 		}
 	}
 	else
-		limited_routine(main, i);
+	{
+		while (i < main->number_of_philosophers)
+		{
+			pthread_create(&main->philo_list[i]->thread, NULL,
+				limited_routine, main->philo_list[i]);
+			i++;
+		}
+	}
 }
