016b2f8d445f0a178051e226d6cf5ff534224115
Saiban0 <bartched@outlook.fr>
2024-09-09 19:45:16 +0200

    ~ | new errors management on main function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:50:01 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/06 15:27:44 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/09 19:39:47 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ int	main(int argc, char **argv)
 	main = malloc(sizeof(t_main));
 	if (!main)
 		return (-1);
-	if (argc < 4)
-		return (1);
+	if (argc != 5 && argc != 6)
+		error_management(e_args, main);
 	init_philo(main, argc, argv);
 	philo_loop(main);
 	philo_end(main);
+	error_management(e_none, main);
 	return (0);
 }
