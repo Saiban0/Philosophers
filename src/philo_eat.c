04af2ed8b44f09cd85143cbcae22d8cddb73e444
Saiban0 <bartched@outlook.fr>
2024-09-12 18:13:39 +0200

    ~ | added some deeadlocks and now use new message function

--- src/philo_eat.c
+++ src/philo_eat.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 20:20:42 by bchedru           #+#    #+#             */
-/*   Updated: 2024/09/09 19:37:01 by bchedru          ###   ########.fr       */
+/*   Updated: 2024/09/12 17:52:55 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,56 @@
 
 static void	grab_forks_last_philo(t_main *main, t_philo *philo)
 {
-	struct timeval	time;
-
 	if (philo->forks_held < 2)
 	{
+		pthread_mutex_lock(&philo->mutex);
 		if (main->fork_list[philo->id - 1]->available
 			&& main->fork_list[0]->available)
 		{
 			pthread_mutex_lock(&main->fork_list[philo->id - 1]->mutex);
 			pthread_mutex_lock(&main->fork_list[0]->mutex);
-			gettimeofday(&time, NULL);
-			ft_printf("%d %d has taken a fork\n", (time.tv_sec / 1000), philo->id);
-			gettimeofday(&time, NULL);
-			ft_printf("%d %d has taken a fork\n", (time.tv_sec / 1000), philo->id);
+			time_message(philo, 'f');
+			time_message(philo, 'f');
 			main->fork_list[philo->id - 1]->available = 0;
-			main->fork_list[philo->id - 1]->used_by = main->tmp;
+			main->fork_list[philo->id - 1]->used_by = philo->id;
 			main->fork_list[0]->available = 0;
-			main->fork_list[0]->used_by = main->tmp;
+			main->fork_list[0]->used_by = philo->id;
 			philo->forks_held = 2;
+			pthread_mutex_unlock(&philo->mutex);
+			philo_eat(main, philo);
+		}
+		else
+		{
+			pthread_mutex_lock(&philo->mutex);
 		}
 	}
 }
 
 static void	grab_forks(t_main *main, t_philo *philo)
 {
-	struct timeval	time;
-
 	if (philo->forks_held < 2)
 	{
+		pthread_mutex_lock(&main->fork_list[philo->id - 1]->mutex);
+		pthread_mutex_lock(&main->fork_list[philo->id]->mutex);
+		pthread_mutex_lock(&philo->mutex);
 		if (main->fork_list[philo->id - 1]->available
 			&& main->fork_list[philo->id]->available)
 		{
-			pthread_mutex_lock(&main->fork_list[philo->id -1]->mutex);
-			pthread_mutex_lock(&main->fork_list[philo->id]->mutex);
-			gettimeofday(&time, NULL);
-			ft_printf("%d %d has taken a fork\n", (time.tv_sec / 1000),
-				philo->id);
-			gettimeofday(&time, NULL);
-			ft_printf("%d %d has taken a fork\n", (time.tv_sec / 1000),
-				philo->id);
+			time_message(philo, 'f');
+			time_message(philo, 'f');
 			main->fork_list[philo->id - 1]->available = 0;
-			main->fork_list[philo->id - 1]->used_by = main->tmp;
+			main->fork_list[philo->id - 1]->used_by = philo->id;
 			main->fork_list[philo->id]->available = 0;
-			main->fork_list[philo->id]->used_by = main->tmp;
+			main->fork_list[philo->id]->used_by = philo->id;
 			philo->forks_held = 2;
+			pthread_mutex_unlock(&philo->mutex);
 			philo_eat(main, philo);
+		}
+		else
+		{
+			pthread_mutex_unlock(&main->fork_list[philo->id - 1]->mutex);
+			pthread_mutex_unlock(&main->fork_list[philo->id]->mutex);
+			pthread_mutex_unlock(&philo->mutex);
 		}
 	}
 }
@@ -70,13 +75,15 @@ void	put_back_forks(t_main *main, t_philo *philo)
 	i = 0;
 	while (main->fork_list[i] && philo->forks_held > 0)
 	{
+		pthread_mutex_lock(&philo->mutex);
 		if (main->fork_list[i]->used_by == philo->id)
 		{
-			pthread_mutex_unlock(&main->fork_list[i]->mutex);
 			main->fork_list[i]->available = 1;
 			main->fork_list[i]->used_by = -1;
 			philo->forks_held--;
+			pthread_mutex_unlock(&main->fork_list[i]->mutex);
 		}
+		pthread_mutex_unlock(&philo->mutex);
 		i++;
 	}
 }
@@ -85,18 +92,19 @@ void	philo_eat(t_main *main, t_philo *philo)
 {
 	struct timeval	time;
 
-	if (main->number_of_times_each_philosophers_must_eat != -1 &&
-		philo->times_eaten >= main->number_of_times_each_philosophers_must_eat)
-	{
+	if (main->number_of_times_each_philosophers_must_eat != -1
+		&& philo->times_eaten
+		>= main->number_of_times_each_philosophers_must_eat)
 		return ;
-	}
 	if (philo->forks_held == 2)
 	{
+		pthread_mutex_lock(&philo->mutex);
+		time_message(philo, 'e');
+		usleep(main->time_to_eat * 1000);
 		gettimeofday(&time, NULL);
-		ft_printf("%d %d is eating\n", (time.tv_sec / 1000), philo->id);
-		usleep(main->time_to_eat);
-		philo->last_meal = (time.tv_sec / 1000);
+		philo->last_meal = ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 		philo->times_eaten++;
+		pthread_mutex_unlock(&philo->mutex);
 		put_back_forks(main, philo);
 	}
 	else
